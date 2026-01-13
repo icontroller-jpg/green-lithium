8633d265a61bba85d265861d89c455595ac482ba
Tadiwanashe Walter Nyika <walternyika07@gmail.com>
2026-01-13 23:23:17 +0530

    Deploy actual frontend as homepage on Render

--- green-lithium/vite.config.js
+++ green-lithium/vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
+    base: "/", // ensure assets are loaded correctly
+    build: {
+        outDir: "public", // default, just to be explicit
+    },
     server: {
         proxy: {
-            "/api": "http://127.0.0.1:8000", // forward all /api requests to Django
+            "/api": "http://127.0.0.1:8000", // ONLY used in dev
         },
     },
 });
