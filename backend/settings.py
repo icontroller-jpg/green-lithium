784d4b79826938c65553be7107db2ac2468f8c61
Tadiwanashe Walter Nyika <walternyika07@gmail.com>
2026-01-13 19:45:02 +0530

    Ignore IDE, virtualenv, media and node modules

--- backend/settings.py
+++ backend/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+
+
     # Third-party
     "rest_framework",
     "corsheaders",
@@ -49,8 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ------------------------------------------------------------------------------
 # CORS
 # ------------------------------------------------------------------------------
