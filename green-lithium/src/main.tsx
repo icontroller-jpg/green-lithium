b70d5d6a301969f9e7d3fead0c7dd11640ee8e6d
Tadiwanashe Walter Nyika <walternyika07@gmail.com>
2026-01-19 19:50:50 +0530

    Fix SellerSignup to use apiFetch

--- green-lithium/src/main.tsx
+++ green-lithium/src/main.tsx
@@ -2,9 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { loadConfig } from "./services/api";
 
-await loadConfig();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
